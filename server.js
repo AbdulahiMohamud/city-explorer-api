938d518eb38dc9ac3beb9eed095e508b30261428
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2022-04-20 22:12:02 -0700

    added the movie and weather api

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ require('dotenv').config();
 const weatherData = require('./data/weather.json');
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 // ALLOWS SHARING BETWEEN MULTIPLE COMPUTERS
 const cors = require('cors');
 const { response } = require('express');
+const res = require('express/lib/response');
 
 app.use(cors());
 
@@ -22,17 +24,38 @@ app.use(cors());
 // define PORT and validate that my .env file is working
 const PORT = process.env.PORT || 3002;
 // if my server is running on 3002, I know something is wrong with my .env file or how I'm importing the values from it.
+app.get('/' , (req,res) => {
+  res.send('Work in progress');
+})
 
-app.get('/weather' ,(request,response) => {
+app.get('/weather' , async (request,response) => {
   
   let searchQueryCity = request.query.searchQueryCity;
-  let cityWeather = weatherData.find((e) => e.city_name.toLowerCase() === searchQueryCity.toLowerCase());
-  let selectedCity = cityWeather.data.map(dailyWeather => {
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQueryCity}&key=${process.env.WEATHER_API_KEY}&days=3&lat=23&lon=155`;
+  let cityWeather = await axios.get(url);
+  let selectedCity = cityWeather.data.data.map(dailyWeather => {
     return new Forecast(dailyWeather);
   });
 
   response.send(selectedCity);
 
+})
+
+app.get('/movies' ,async (req,res) => {
+  
+  let movieQueryCity = req.query.movieQueryCity;
+
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${movieQueryCity}`
+ 
+  let cityMovie = await axios.get(url);
+  
+  
+  let selectedMovie = cityMovie.data.results.map(dailyMovie => {
+    return new Movie(dailyMovie);
+  });
+
+
+  res.send(selectedMovie);
 })
 
 
@@ -45,6 +68,18 @@ class Forecast {
   constructor(cityWeather) {
     this.date = cityWeather.datetime;
     this.description = cityWeather.weather.description;
+  }
+}
+
+class Movie {
+  constructor(cityMovie) {
+    this.title = cityMovie.original_title;
+    this.description = cityMovie.overview;
+    this.avgVotes = cityMovie.vote_average;
+    this.totalVotes = cityMovie.vote_count;
+    this.popularity = cityMovie.popularity;
+    this.releasedOn = cityMovie.released_date;
+    this.img = cityMovie.poster_path;
   }
 }
 
